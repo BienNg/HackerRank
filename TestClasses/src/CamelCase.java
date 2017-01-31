94e933668197e98f0c90db3daafb3706e91f8768
Bien Nguyen <bien.nguyen@hotmail.de>
2017-01-31 18:42:40 +0100

    Algorithms -> Strings

---
+++ TestClasses/src/CamelCase.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by bien on 31.01.2017.
+ */
+public class CamelCase {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+
+        char[] inputArray = s.toCharArray();
+        int wordCounter = 1;
+        for(char c:inputArray){
+            if(Character.isUpperCase(c)){
+                wordCounter++;
+            }
+        }
+        System.out.println(wordCounter);
+    }
+}
---
+++ TestClasses/src/SuperReduceString.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by bien on 31.01.2017.
+ */
+public class SuperReduceString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        char[] chars = input.toCharArray();
+
+        ArrayList<Character> arrayList = new ArrayList<>();
+
+        for(char a : chars){
+            arrayList.add(a);
+        }
+
+        for(int i=0 ; i<arrayList.size()-1 ; i++){
+            if(arrayList.get(i) == arrayList.get(i+1)){
+                arrayList.remove(i+1);
+                arrayList.remove(i);
+                i=-1;
+            }
+        }
+
+        String output = "";
+        if(!arrayList.isEmpty()){
+            for(char c : arrayList){
+                output += c;
+            }
+            System.out.println(output);
+        }else{
+            System.out.println("Empty String");
+        }
+    }
+}
