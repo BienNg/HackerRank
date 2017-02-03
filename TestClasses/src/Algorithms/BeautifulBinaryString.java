713518b35d2ec2852e5f699dc2366780e583f579
BienNg <bien.nguyen@hotmail.de>
2017-02-03 10:57:53 +0100

    Create BeautifulBinaryString.java

---
+++ TestClasses/src/Algorithms/BeautifulBinaryString.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int length = scan.nextInt();
+        StringBuilder binary = new StringBuilder(scan.next());
+        int counter = 0;
+        for(int i=0 ; i<=binary.length()-3 ; i++){
+            
+            if(binary.charAt(i) == '0' && binary.charAt(i+1) == '1' && binary.charAt(i+2) == '0'){
+                binary.setCharAt(i+2,'1');
+                counter++;
+                i+=2;
+            }
+        }
+        System.out.println(counter);
+    }
+}
