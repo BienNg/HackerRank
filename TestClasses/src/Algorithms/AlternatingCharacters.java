2aea0ddc8fc010fb690da582790c0f786c713446
BienNg <bien.nguyen@hotmail.de>
2017-02-03 10:39:42 +0100

    Create AlternatingCharacters.java

---
+++ TestClasses/src/Algorithms/AlternatingCharacters.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int lines = scan.nextInt();
+        String[] strings = new String[lines];
+        int[] deletionCounter = new int[lines];
+        int deletions = 0;
+        for(int i=0 ; i<lines ; i++){
+            strings[i] = scan.next();
+        }
+        for(String s : strings){
+            StringBuilder sb = new StringBuilder(s);
+            for(int i=0 ; i<s.length()-1 ; i++){
+                if(s.charAt(i) == s.charAt(i+1)){
+                    sb.deleteCharAt(i+1);
+                    s = sb.toString();
+                    deletions++;
+                    i--;
+                }
+            }
+            System.out.println(deletions);
+            deletions = 0;
+        }
+    }
+}
