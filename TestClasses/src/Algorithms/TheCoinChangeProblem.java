968413675cab79624658a9a755fdc19972758098
BienNg <bien.nguyen@hotmail.de>
2017-02-03 13:45:31 +0100

    Create TheCoinChangeProblem.java
    unsolved

---
+++ TestClasses/src/Algorithms/TheCoinChangeProblem.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int amount = scan.nextInt();
+        int size = scan.nextInt();
+        int[] changes = new int[size];
+        for( int i=0 ; i<size ; i++){
+            changes[i] = scan.nextInt();
+        }
+        if(amount <= 0 || size <= 0 || changes[0] > amount){
+            System.out.println("0");
+        }else{
+            int change = 0;
+            ArrayList<Integer> changeList = new ArrayList<>();
+            while(change < amount){
+                
+            }
+        }
+    }
+}
