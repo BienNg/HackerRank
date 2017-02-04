4a9c434b80edfdb739b93248849d6628e0493cf7
Bien Nguyen <bien.nguyen@hotmail.de>
2017-02-04 02:06:31 +0100

    Cruscal 2 failed test cases

--- TestClasses/src/Algorithms/BeautifulBinaryString.java
+++ TestClasses/src/Algorithms/BeautifulBinaryString.java
@@ -1,7 +1,10 @@
+package Algorithms;
+
+
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class BeautifulBinaryString {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
--- TestClasses/src/Algorithms/KruskalReallySpecialSubtree.java
+++ TestClasses/src/Algorithms/KruskalReallySpecialSubtree.java
@@ -1,7 +1,9 @@
+package Algorithms;
+
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class KruskalReallySpecialSubtree {
     
     
     static class Edge{
@@ -26,14 +28,49 @@ public class Solution {
         for(int i = 0; i<edgeCounter ;i++){
             edgeList[i] = new Edge(scan.nextInt(), scan.nextInt(), scan.nextInt());
         }
-        for(int i=0 ; i<edgeList.length-i+1 ; i++;){
-            Edge tempE = edgeList[j];
-            edgeList[j] = edgeList[j+1];
-            edgeList[j+1] = tempE;
+
+        //Sort the edgeList
+        boolean listNotSorted;
+        do{
+            listNotSorted = false;
+            int j = 0;
+            for(int i=0 ; i<edgeList.length-j-1 ; i++){
+                if(edgeList[i].weight > edgeList[i+1].weight){
+                    Edge tempE = edgeList[i];
+                    edgeList[i] = edgeList[i+1];
+                    edgeList[i+1] = tempE;
+                    listNotSorted = true;
+                }
+            }
+            j++;
+        }while(listNotSorted);
+
+
+        ArrayList<Edge> specialTree = new ArrayList<>();
+        specialTree.add(edgeList[0]);
+        boolean[] nodeList = new boolean[nodeCounter];
+        int weightCounter = 0;
+        for(Edge edge : edgeList){
+            if(!nodeList[edge.startNode-1] && !nodeList[edge.endNode-1]){
+                nodeList[edge.startNode-1] = true;
+                nodeList[edge.endNode-1] = true;
+                weightCounter += edge.weight;
+                //System.out.println(edge.startNode + " - " + edge.endNode);
+            }
+            else if(!nodeList[edge.endNode-1]){
+                nodeList[edge.endNode-1] = true;
+                weightCounter += edge.weight;
+                //System.out.println(edge.startNode + " - " + edge.endNode);
+            } else if(!nodeList[edge.startNode-1]) {
+                nodeList[edge.startNode - 1] = true;
+                weightCounter += edge.weight;
+                //System.out.println(edge.startNode + " - " + edge.endNode);
+            }
         }
-            
-        for(Edge e : edgeList){
-            System.out.println(e.weight);
+
+        System.out.println(weightCounter);
+        for(Edge i : edgeList){
+            System.out.println(i.weight);
         }
     }
 }
